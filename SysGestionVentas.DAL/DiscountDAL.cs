97f55d31baf30dc81c368ed3f20869b03a29121c
Siguachi25 <sp25002@esfe.agape.edu.sv>
2026-03-21 15:50:21 -0600

    Trabaje en las clases

---
+++ SysGestionVentas.DAL/DiscountDAL.cs
@@ -0,0 +1,25 @@
+﻿using SysGestionVentas.EN;
+using Microsoft.EntityFrameworkCore;
+using SysGestionVentas.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysGestionVentas.DAL
+{
+    public class DiscountDAL
+    {
+        public static async Task<int> GuardarAsync(Discount discount, DbContexto dbContexto)
+        {
+            dbContexto.Add(discount);
+            return await dbContexto.SaveChangesAsync();
+        }
+
+        public static async Task<Discount> ObtenerPorIdAsync(int id, DbContexto dbContexto)
+        {
+            return await dbContexto.Discount.FirstOrDefaultAsync(d => d.Id == id);
+        }
+    }
+}
---
+++ SysGestionVentas.DAL/InventoryMovementDAL.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SysGestionVentas.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysGestionVentas.DAL
+{
+    public class InventoryMovementDAL
+    {
+        public static async Task<int> RegistrarMovimientoAsync(InventoryMovement movement, DbContexto dbContexto)
+        {
+            dbContexto.Add(movement);
+            return await dbContexto.SaveChangesAsync();
+        }
+
+        public static async Task<List<InventoryMovement>> ConsultarPorProductoAsync(int idProducto, DbContexto dbContexto)
+        {
+            return await dbContexto.InventoryMovement
+                .Where(m => m.IdProduct == idProducto)
+                .OrderByDescending(m => m.RegistrationDate)
+                .ToListAsync();
+        }
+    }
+}
