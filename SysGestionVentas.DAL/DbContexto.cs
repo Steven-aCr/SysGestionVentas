7bf1b9a2190cb5de57ca5b09dba946b8564774db
Steven-aCr <stevenrecinosac@gmail.com>
2026-03-22 23:37:59 -0600

    feat(dal): add Department, MovementType, ProductDiscount and RolPermission repositories with EF configurations

--- SysGestionVentas.DAL/DbContexto.cs
+++ SysGestionVentas.DAL/DbContexto.cs
@@ -22,13 +22,26 @@ namespace SysGestionVentas.DAL
         public DbSet<Inventory> Inventory { get; set; }
         public DbSet<InventoryMovement> InventoryMovement { get; set; }
         public DbSet<Discount> Discount { get; set; }
-        public DbSet<ProductDiscout> ProductDiscout { get; set; }
+        public DbSet<ProductDiscount> ProductDiscount { get; set; }
         public DbSet<DocumentType> DocumentType { get; set; }
         public DbSet<Document> Document { get; set; }
         public DbSet<DocumentDetail> DocumentDetail { get; set; }
         public DbSet<Department> Department { get; set; }
         public DbSet<MovementType> MovementType { get; set; }
 
+        /// <summary>
+        /// Configura el modelo de datos para el contexto actual utilizando las convenciones y las configuraciones
+        /// aplicadas desde el ensamblado actual.
+        /// </summary>
+        /// <remarks>Este método se llama durante la inicialización del contexto para personalizar el
+        /// modelo de Entity Framework. Utiliza todas las configuraciones de entidad definidas en el ensamblado del
+        /// contexto.</remarks>
+        /// <param name="modelBuilder">El generador de modelos utilizado para construir el modelo de datos para este contexto. No puede ser nulo.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DbContexto).Assembly);
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
